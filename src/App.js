966c21832b2a5024f1b000f9ee5464574fba7de8
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-05-31 17:37:59 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Container, Form, Jumbotron } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Form className='text-center'>
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form>
-      <Button> Cadastrar </Button>
+      <Jumbotron>
+        <Container fluid >
+          <Form className='text-center'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form>
+          <Button> Cadastrar </Button>
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
