4d75c7b468df5317001d721ce104fca00e1fab7d
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-06-01 14:00:23 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { Button, Container, Form, Jumbotron } from 'react-bootstrap';
-import './App.css';
+import React, { useRef } from 'react'
+import { Form } from '@unform/web'
 
-function App() {
+import Input from './Form/Input'
+
+const App = () => {
+  const formRef = useRef()
+
+  const handleFormSubmit = data => {
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <Jumbotron>
-        <Container fluid >
-          <Form className='text-center'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form>
-          <Button> Cadastrar </Button>
-        </Container>
-      </Jumbotron>
-    </div>
-  );
+    <Form ref={formRef} onSubmit={handleFormSubmit}>
+      <Input name="name" placeholder="Insert your name"/>
+      <Input name="tel"  placeholder="Tel"/>
+      <Input name="city"  placeholder="City"/>
+      <Input name="state"  placeholder="State"/>
+      <Input name="country"  placeholder="Country"/>
+
+      <button type="submit">Save</button>
+    </Form>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Form/Input.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+import { useField } from '@unform/core'
+
+
+
+const Input = ({ name, ...rest }) => {
+  const inputRef = useRef()
+  const { fieldName, defaultValue, registerField } = useField(name)
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: ref => {
+        return ref.current.value
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value
+      },
+      clearValue: ref => {
+        ref.current.value = ''
+      },
+    })
+  }, [fieldName, registerField])
+
+  return (
+    <div>
+    <input
+      name={name}
+      ref={inputRef}
+      type="text"
+      placeholder="Type your username"
+      {...rest}
+    />
+    </div>
+  )
+}
+
+export default Input
