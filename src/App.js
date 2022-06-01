c1aef229cff4e02705b529599438744f819aefdf
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-06-01 16:41:53 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react'
 import { Form } from '@unform/web'
-
+import './App.css'
 import Input from './Form/Input'
 import Contato from './models/contato.model'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, FormLabel } from 'react-bootstrap'
 
 const App = () => {
   const formRef = useRef(null)
-
 
   const handleFormSubmit = data => {
     console.log(data);
@@ -20,15 +21,18 @@ const App = () => {
   }
 
   return (
-    <Form ref={formRef} onSubmit={handleFormSubmit}>
-      <Input name="name" placeholder="Insert your name"/>
-      <Input name="tel"  placeholder="Tel"/>
-      <Input name="city"  placeholder="City"/>
-      <Input name="state"  placeholder="State"/>
-      <Input name="country"  placeholder="Country"/>
+    <div className='text-center'>
+      <Form ref={formRef} onSubmit={handleFormSubmit} className="justify-between">
+        <FormLabel>Cadastro:</FormLabel>
+        <Input className='mt-2' name="name" placeholder="Insert your name"/>
+        <Input className='mt-2' name="tel"  placeholder="Tel"/>
+        <Input className='mt-2' name="city"  placeholder="City"/>
+        <Input className='mt-2' name="state"  placeholder="State"/>
+        <Input className='mt-2'name="country"  placeholder="Country"/>
 
-      <button type="submit">Save</button>
-    </Form>
+        <Button className='mt-2' variant="danger"type="submit">Save</Button>
+      </Form>
+    </div>
   )
 }
 
