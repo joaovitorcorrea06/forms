cba644308a1f6d7e334e7ac766cbfd20b53ebf69
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-06-02 13:25:42 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css'
 import Input from './Form/Input'
 import Contato from './models/contato.model'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Container, FormLabel, Table } from 'react-bootstrap'
+import { Button, Container, FormLabel, Modal, Table } from 'react-bootstrap'
 import TableContato from './table/table'
 
 const App = () => {
 
-  const[listaContatos, setListaContatos] = useState([]);
+  const [listaContatos, setListaContatos] = useState([]);
+  const [exibirModal, setExibirModal] = useState (false);
+
+  function handleAbrirModal(event) {
+    event.preventDefault();
+    setExibirModal(true);
+  }
+
+  function handleFecharModal(){
+      setExibirModal(false);
+  }
 
   const formRef = useRef(null)
 
   const handleFormSubmit = data => {
-    // console.log(data);
+    console.log(data);
 
     let contatosDb = JSON.parse(localStorage['contatos']);
+
     if (!contatosDb){
       contatosDb = [];
-    }
+    } 
   
     contatosDb.push(data);
     //  const contatos = contatosDb ? JSON.parse(contatosDb) : [];
@@ -33,21 +44,29 @@ const App = () => {
     formRef.current.setFieldValue('city', '');
     formRef.current.setFieldValue('state', '');
     formRef.current.setFieldValue('country', '');
+    setExibirModal(false);
 
   }
 
   return (
     <div className='text-center'>
-      <Form ref={formRef} onSubmit={handleFormSubmit} className="justify-between">
-        <FormLabel>Cadastro:</FormLabel>
-        <Input className='mt-2' name="name" placeholder="Insert your name"/>
-        <Input className='mt-2' name="tel" type = "tel"  placeholder="Tel"/>
-        <Input className='mt-2' name="city"  placeholder="City"/>
-        <Input className='mt-2' name="state"  placeholder="State"/>
-        <Input className='mt-2' name="country"  placeholder="Country"/>
+      <Button onClick={handleAbrirModal} className='mt-2 mb-2'>Cadastrar</Button>
 
-        <Button className='mt-2' variant="danger"type="submit">Save</Button>
-      </Form>
+      <Modal show={exibirModal}  onHide={handleFecharModal} className='text-center' >
+      <Modal.Header closeButton>
+          <Modal.Title>Register:</Modal.Title>
+      </Modal.Header>
+        <Form ref={formRef} onSubmit={handleFormSubmit} className="justify-between">
+          <FormLabel>Info:</FormLabel>
+          <Input className='mt-2' name="name"  placeholder="Insert your name"/>
+          <Input className='mt-2' name="tel" type = "tel"  placeholder="Tel"/>
+          <Input className='mt-2' name="city"  placeholder="City"/>
+          <Input className='mt-2' name="state" placeholder="State"/>
+          <Input className='mt-2' name="country"  placeholder="Country"/>
+
+          <Button className='mt-2 mb-2' variant="danger" type="submit">Save</Button>
+        </Form>
+      </Modal>
       <TableContato dados={listaContatos} />
     </div>
   )
--- src/table/table.js
+++ src/table/table.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
-import {Table, Container} from "react-bootstrap";
+import {Table, Container, Button, Modal, FormLabel, Form} from "react-bootstrap";
+import Input from "../Form/Input";
 
 function TableContato (props) {
 
-    const [carregar, setCarregar] = useState(true);
+    const [exibirModal, setExibirModal] = useState (false);
 
+    function handleAbrirModal(event) {
+      event.preventDefault();
+      setExibirModal(true);
+    }
+    
+    function handleFecharModal(){
+      setExibirModal(false);
+    }
 
+    function deleteIndex(dados){
+      localStorage.removeItem(dados);
+
+    }
 
     let listaContatos = JSON.parse(localStorage.getItem('contatos'));
 
@@ -13,7 +26,7 @@ function TableContato (props) {
 
     return(
     <Container fluid>
-        <Table bordered striped hover>
+        <Table bordered striped hover variant="dark">
         <thead>
           <tr>
             <th>Name</th>
@@ -21,6 +34,8 @@ function TableContato (props) {
             <th>City</th>
             <th>State</th>
             <th>Country</th>
+            <th>All</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -31,14 +46,37 @@ function TableContato (props) {
             <td>{dados.city}</td>
             <td>{dados.state}</td>
             <td>{dados.country}</td>
+            <td><Button
+            onClick={handleAbrirModal}
+            >?</Button></td>
+
+            <td><Button variant="danger"
+            onClick={deleteIndex}
+            >X</Button></td>
+            
+            <Modal show={exibirModal}  onHide={handleFecharModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>DADOS:</Modal.Title>
+            </Modal.Header>
+              <Form className="justify-between">
+                <FormLabel>Cadastro:</FormLabel>
+                {/* <Input className='mt-2' name="name" type = "text" placeholder={dados.name}/>
+                <Input className='mt-2' name="tel" type = "tel"  placeholder={dados.tel}/>
+                <Input className='mt-2' name="city" type = "text" placeholder={dados.city}/>
+                <Input className='mt-2' name="state" type = "text" placeholder={dados.state}/>
+                <Input className='mt-2' name="country" type = "text" placeholder={dados.country}/> */}
+
+                {/* <Button className='mt-2 mb-2' variant="danger"type="submit">Save</Button> */}
+              </Form>
+            </Modal>
             </tr>
           )}
+
             {/* <td>{listaContatos.name}</td>
             <td>{listaContatos.tel}</td>
             <td>{listaContatos.city}</td>
             <td>{listaContatos.state}</td>
             <td>{listaContatos.country}</td> */}
-            {/* <td>oi</td> */}
         </tbody>
         </Table>
       </Container>
