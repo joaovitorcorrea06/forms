2a53b6fa0308134fb4b82eeac10dc89abd895e4d
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-06-07 07:58:00 -0300

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contato from './models/contato.model'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, FormLabel, Modal } from 'react-bootstrap'
 import TableContato from './table/table'
+import ModalRegister from './modal'
 
 const App = () => {
 
@@ -36,49 +37,10 @@ const App = () => {
   }
 
   function handleAbrirModal(event) {
-    event.preventDefault();
     setExibirModal(true);
 
   }
 
-  function handleFecharModal(){
-      setExibirModal(false);
-  }
-
-  const formRef = useRef(null)
-
-  const handleFormSubmit = data => {
-    console.log(data);
-
-    let contatosDb= localStorage['contatos'];
-    // let contatosDb = JSON.parse(localStorage['contatos']);
-
-    if (!contatosDb){
-      contatosDb = [];
-      setShowTable(false);
-    } 
-    else{
-       contatosDb = JSON.parse(localStorage['contatos']);
-       setShowTable(true);
-    }
-  
-    contatosDb.push(data);
-    //  const contatos = contatosDb ? JSON.parse(contatosDb) : [];
-
-    //  contatos.push(new Contato(formRef.getData(data)));
-    localStorage['contatos'] = JSON.stringify(contatosDb);
-    setListaContatos(contatosDb);
-
-    formRef.current.setFieldValue('id','');
-    formRef.current.setFieldValue('name', '');
-    formRef.current.setFieldValue('tel', '');
-    formRef.current.setFieldValue('city', '');
-    formRef.current.setFieldValue('state', '');
-    formRef.current.setFieldValue('country', '');
-    setExibirModal(false);
-    // setId({id}+1);
-
-  }
 
   return ( 
     <div  className='text-center'>
@@ -86,21 +48,8 @@ const App = () => {
       <Button onClick={handleAbrirModal} className='mt-2 mb-2 ' size="lg">Register</Button>
       </div>
 
-      <Modal show={exibirModal}  onHide={handleFecharModal} className='text-center' >
-      <Modal.Header closeButton>
-          <Modal.Title>Register:</Modal.Title>
-      </Modal.Header>
-        <Form ref={formRef} onSubmit={handleFormSubmit} className="justify-between">
-          <FormLabel>Data:</FormLabel>
-          <Input className='mt-2' name="name"  placeholder="Insert your name"/>
-          <Input className='mt-2' name="tel" type = "tel"  placeholder="Tel"/>
-          <Input className='mt-2' name="city"  placeholder="City"/>
-          <Input className='mt-2' name="state" placeholder="State"/>
-          <Input className='mt-2' name="country"  placeholder="Country"/>
+      <ModalRegister exibirModal={exibirModal}/>
 
-          <Button className='mt-2 mb-2' variant="danger" type="submit">Save</Button>
-        </Form>
-      </Modal>
       {showTable ? <TableContato dados={listaContatos}></TableContato> : 
       <div>
       <h1>Your list is empty!</h1>
---
+++ src/modal.js
@@ -0,0 +1,65 @@
+import { useRef, useState } from "react";
+import { Button, Form, FormLabel, Modal } from "react-bootstrap";
+import Input from "./Form/Input";
+
+function ModalRegister(props){
+
+    const [exibirModal, setExibirModal] = useState (false);
+    const [listaContatos, setListaContatos] = useState([]);
+
+    function handleFecharModal(){
+        setExibirModal(false);
+    }
+  
+    // const formRef = useRef(null)
+
+    const handleFormSubmit = data => {
+    
+        let contatosDb= localStorage['contatos'];
+        // let contatosDb = JSON.parse(localStorage['contatos']);
+    
+        if (!contatosDb){
+          contatosDb = [];
+        } 
+        else{
+           contatosDb = JSON.parse(localStorage['contatos']);
+        }
+      
+        contatosDb.push(data);
+        //  const contatos = contatosDb ? JSON.parse(contatosDb) : [];
+    
+        //  contatos.push(new Contato(formRef.getData(data)));
+        localStorage['contatos'] = JSON.stringify(contatosDb);
+        setListaContatos(contatosDb);
+    
+        // formRef.current.setFieldValue('id','');
+        // formRef.current.setFieldValue('name', '');
+        // formRef.current.setFieldValue('tel', '');
+        // formRef.current.setFieldValue('city', '');
+        // formRef.current.setFieldValue('state', '');
+        // formRef.current.setFieldValue('country', '');
+        setExibirModal(false);
+        // setId({id}+1);
+    
+      }
+
+
+return(
+    <Modal show={props.exibirModal}  onHide={handleFecharModal} className='text-center' >
+       <Modal.Header closeButton>
+           <Modal.Title>Register:</Modal.Title>
+       </Modal.Header>
+         <Form onSubmit={handleFormSubmit} className="justify-between">:
+           <FormLabel>Data:</FormLabel>
+           <Input className='mt-2' name="name"  placeholder="I000nsert your name"/>
+           <Input className='mt-2' name="tel" type = "tel"  placeholder="Tel"/>
+           <Input className='mt-2' name="city"  placeholder="City"/>
+           <Input className='mt-2' name="state" placeholder="State"/>
+           <Input className='mt-2' name="country"  placeholder="Country"/> 
+
+           <Button className='mt-2 mb-2' variant="danger" type="submit">Save</Button>
+        </Form>
+      </Modal>
+)
+}
+export default ModalRegister;
